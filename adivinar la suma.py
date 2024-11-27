4308d49da16d5dcddd5349833185412f1f12ca04
jose-sacilotto <joseignaciosacilotto@gmail.com>
2024-11-27 14:57:49 -0300

    cambios 5

--- adivinar la suma.py
+++ adivinar la suma.py
@@ -1,21 +1,15 @@
 import random
-def generarmatriz(filas, columnas):
-    return [[random.randint(1, 10) for _ in range(columnas)] for _ in range(filas)]
-def imprimirmatriz(matriz):
-    for fila in matriz:
-        print(" ".join(map(str, fila)))
-def juegosumamatriz():
-    filas = 3
-    columnas = 3
-    matriz = generarmatriz(filas, columnas)
-    imprimirmatriz(matriz)
-    suma = 0
-    for fila in matriz:
-        for valor in fila:
-            suma += valor
-    intento = int(input(f"¿Cuál es la suma de todos los números en la matriz de {filas}x{columnas}? "))
-    if intento == suma:
-        print("¡Correcto!")
-    else:
-        print(f"¡Incorrecto! La suma correcta es {suma}. El juego ha terminado.")
-juegosumamatriz()
+filas = 3
+columnas = 3
+suma = 0
+matriz = [[random.randint(1, 10) for _ in range(columnas)] for _ in range(filas)]
+for fila in matriz:
+    print(" ".join(map(str, fila)))
+for fila in matriz:
+    for valor in fila:
+        suma += valor
+intento = int(input(f"¿Cuál es la suma de todos los números en la matriz de {filas}x{columnas}? "))
+if intento == suma:
+    print("¡Correcto!")
+else:
+    print(f"¡Incorrecto! La suma correcta es {suma}. El juego ha terminado.")
